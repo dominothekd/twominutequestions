0399de74d83006e06c9348e839dae656f66e196b
dominothekd <80610006+dominothekd@users.noreply.github.com>
2021-06-17 09:06:38 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,38 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "If M and A represent integers, M + A = A + M is an example of which property?",
+    answer: "The Commutative Property",
+    options: [
+      "The Commutative Property",
+      "The Associative Propety",
+      "The Distributive Propery"
+
+    ]
+  },
+    {
+    numb: 2,
+    question: "Simplify: 5 + 3²- 7 + 1",
+    answer: "8",
+    options: [
+      "16",
+      "8",
+      "32"
+    ]
+  },
+]
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // }
